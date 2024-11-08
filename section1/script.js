cb72d39d39bf5c9ded9b0591a7078c99f488ebc8
Parth-Ramani <parthramani93@gmail.com>
2024-11-08 19:36:12 +0530

    written section 1 of js

---
+++ section1/script.js
@@ -0,0 +1,28 @@
+ 
+ let x=5;
+  x=+15
+  x*= 5; // x= x*5
+  console.log( x); // Output: 75
+let y="7" -3
+  console.log(typeof y)
+
+
+
+  // concatenation with number
+  let n ="1"+ 1 //string concatenation "11"
+  n-=1 // n= n-1 n=10
+  console.log(typeof n) // Output: "10"
+
+
+  let concatenation= "15"- "5" +1 // concatenation is performed first, then subtraction
+  console.log(concatenation) // Output: 11
+
+  let concatenation2= "10" - "4" - "3" -2 + '5' // operations are performed from left to right and string concatenation is performed after all other operations
+  console.log(concatenation2) // Output: 12
+
+
+
+  // truly and falsey values
+
+  // falsey values: false, 0, "", null, undefined, NaN
+  // truthy values: true, any number that is not falsey, any array, any object, any function

a5bb2cf7b4e7c15f4e116254d3190d2e85f7df76
Parth-Ramani <parthramani93@gmail.com>
2024-11-10 20:22:13 +0530

    started the dom with project

---
+++ section_5_guess_my_number/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+document.querySelector('.check').addEventListener('click', () => {
+const guess = Number(document.querySelector('.guess').value);
+console.log(guess);    
+if(!guess){
+document.querySelector('.message').textContent = 'Please enter a number';
+}
+
+})

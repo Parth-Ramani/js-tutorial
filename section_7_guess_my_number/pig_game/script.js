a84f2be50458ed18d50a81e06ae170f6a6bbf520
Parth-Ramani <parthramani93@gmail.com>
2024-11-13 12:03:49 +0530

    completed the game of the pig game with added last functionlity of reset game

--- section_7_guess_my_number/pig_game/script.js
+++ section_7_guess_my_number/pig_game/script.js
@@ -18,6 +18,14 @@ let scores = [0, 0]// Array for scores of both players
 let currentScore = 0
 let activePlayer = 0
 
+function switchPlayer(){
+    document.getElementById(`current--${activePlayer}`).textContent = 0 // Reset current score to 0
+    currentScore = 0
+activePlayer = activePlayer === 0?1:0 // Switch active player
+player0El.classList.toggle('player--active')
+player1El.classList.toggle('player--active')
+}
+
 btnRollDiceBtn.addEventListener('click',()=>{
     let dice = Math.trunc(Math.random() * 6) + 1
 console.log(dice)
@@ -28,11 +36,7 @@ console.log(dice)
         currentScore += dice// Add dice value to current score
 document.getElementById(`current--${activePlayer}`).textContent = currentScore // Update current score on UI
     }else{
-        document.getElementById(`current--${activePlayer}`).textContent = 0 // Reset current score to 0
-        currentScore = 0
-activePlayer = activePlayer === 0?1:0 // Switch active player
-player0El.classList.toggle('player--active')
-player1El.classList.toggle('player--active')
+        switchPlayer() // Switch player if dice is 1
         
     }
     
@@ -40,8 +44,45 @@ player1El.classList.toggle('player--active')
 
 
 btnHoldBtn.addEventListener('click',()=>{
-document.getElementById(`score--${activePlayer}`).textContent =  currentScore
-activePlayer = activePlayer === 0?1:0 // Switch active player
-player0El.classList.toggle('player--active')
-player1El.classList.toggle('player--active')
+
+    // Add current score to active player's score
+scores[activePlayer] += currentScore // Add current score to active player's score
+// score[1] = score[1] =currentScore
+
+document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer] // Update score on UI
+
+
+if(scores[activePlayer] >= 20){
+    document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
+    diceEl.classList.add('hidden')
+    btnRollDiceBtn.disabled = true
+    btnHoldBtn.disabled = true
+}else{
+    switchPlayer() // Switch player if current score is 20
+}
+})
+
+
+
+btnNewGameBtn.addEventListener('click',()=>{
+    scores = [0, 0] // Reset scores
+    currentScore = 0
+    activePlayer = 0
+    score0El.textContent = 0
+    score1El.textContent = 0
+    document.querySelector('.player--0').classList.remove('player--winner')
+    document.querySelector('.player--1').classList.remove('player--winner')
+    document.querySelector('.player--0').classList.add('player--active')
+    document.querySelector('.player--1').classList.remove('player--active')
+    diceEl.classList.add('hidden')
+    btnRollDiceBtn.disabled = false
+    btnHoldBtn.disabled = false
+
+    // diceEl.style.display = 'none'
+
+    current0El.textContent = 0
+    current1El.textContent = 0
+
+
 })

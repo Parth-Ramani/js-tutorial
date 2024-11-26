23cb625a2d8c9b3a373f65a7604ce11bbb79678f
Parth-Ramani <parthramani93@gmail.com>
2024-11-26 10:57:14 +0530

    worked with the loan functionality

--- section_11/script.js
+++ section_11/script.js
@@ -243,6 +243,20 @@ console.log(currentAccount.movements)
   inputTransferTo.value = inputTransferAmount.value = "";
 });
 
+
+btnLoan.addEventListener('click', (e)=>{
+  e.preventDefault();
+
+const amount = Number(inputLoanAmount.value);
+if(amount>0 && currentAccount.movements.some((mov)=>mov>=amount*0.1)){
+  currentAccount.movements.push(amount);
+  updateUI(currentAccount)
+
+}
+inputLoanAmount.value = "";
+})
+
+
 btnClose.addEventListener("click", (e)=>{
 e.preventDefault();
 console.log('delete') 
